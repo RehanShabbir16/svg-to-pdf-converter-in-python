1c3bc79460a5c1c03cac78da53d1b3685e6a701c
RehanShabbir16 <122561375+RehanShabbir16@users.noreply.github.com>
2023-05-05 18:48:56 +0500

    Update svg-to-pdf.py

--- svg-to-pdf.py
+++ svg-to-pdf.py
@@ -18,25 +18,19 @@ directory = 'svgs'
 for filename in os.listdir(directory):
     postcodes = []
     fzip = os.path.join(directory, filename)
-    # checking if it is a file
 
     if os.path.isfile(fzip) and '.zip' in fzip:
-        # print(fzip)
-        print("aa")
+
         with zipfile.ZipFile(fzip, 'r') as zip_ref:
             zip_ref.extractall(fzip.split('.')[0])
             directory_file = fzip.split('.')[0]
-            print(".")
             for ff in os.listdir(directory_file):
                 # print(ff)
                 postcodes = []
                 f = os.path.join(directory_file, ff)
-                # print(f)
-                # checking if it is a file
                 if os.path.isfile(f) and '.svg' in f:
-                    # print(f)
                     wkhtml_path = pdfkit.configuration(wkhtmltopdf=r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe')
 
                     pdfkit.from_file(f, f.split('.')[0] + '.pdf', configuration=wkhtml_path, options=options)
-                    print("...")
+
 
